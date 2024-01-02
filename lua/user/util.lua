3152e3fc9e1d183cd7d8f80ef1bd0469fbf78b4f
danielucho123 <danielucho345@gmail.com>
2024-01-02 12:40:43 -0600

    functions for paths

---
+++ lua/user/util.lua
@@ -0,0 +1,40 @@
+local function relative_file_path()
+ -- local path = vim.fn.expand('%:h') .. '\\' .. vim.fn.expand('%:t')
+ local path = vim.fn.expand('%')
+ vim.fn.setreg("+", path)
+ vim.notify('Copied "' .. path .. '" to the clipboard!')
+
+end
+
+local function full_file_path()
+ local path = vim.fn.expand('%:p')
+ vim.fn.setreg("+", path)
+ vim.notify('Copied "' .. path .. '" to the clipboard!')
+
+end
+
+local function root_file_path()
+ -- Copy the current file working directory  
+
+ local path = vim.fn.expand('%:h')
+ vim.fn.setreg("+", path)
+ vim.notify('Copied "' .. path .. '" to the clipboard!')
+
+end
+
+-- Relative file path
+vim.api.nvim_create_user_command("CpRelFilePath", function()
+ relative_file_path()
+end, {})
+
+-- Full file path 
+vim.api.nvim_create_user_command("CpFullFilePath", function()
+ full_file_path()
+end, {})
+
+-- Root folder of the current file  
+vim.api.nvim_create_user_command("CpRootFilePath", function()
+ root_file_path()
+end, {})
+
+
