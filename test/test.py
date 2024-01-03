e87b6b1aa5309ca40f919f17bddfd286f6e88b07
danielucho123 <danielucho345@gmail.com>
2024-01-03 11:01:57 -0600

    treesitter config
    TSToggle Highlighting

---
+++ test/test.py
@@ -0,0 +1,13 @@
+import datetime
+
+now = datetime.datetime.now()
+
+
+class Myclass(object):
+    def __init__(self) -> None:
+        self.name = 'myname'
+        self.createdat = datetime.datetime.now()
+    
+
+
+
